fd74d2ba323101b9862cbfa4e4fe7178031126a4
hnp435 <namhoangpham96@gmail.com>
2019-07-02 20:22:06 -0400

    Add the style static for template

--- src/data/views.py
+++ src/data/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def table_view(request, *args, **kwargs):
-	return render(request, "data/show_table.html", {})
+	return render(request, "show_table.html", {})
